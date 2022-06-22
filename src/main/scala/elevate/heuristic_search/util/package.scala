18b1b12d5928697a7227bc476477a4cf61c31b1d
Johannes Lenfers <j.le@wwu.de>
2022-06-22 18:41:06 +0200

    change hashing

--- src/main/scala/elevate/heuristic_search/util/package.scala
+++ src/main/scala/elevate/heuristic_search/util/package.scala
@@ -7,13 +7,23 @@ package object util {
 
   val sha256 = MessageDigest.getInstance("SHA-256")
 
+  def convertBytesToHex(bytes: Seq[Byte]): String = {
+    val sb = new StringBuilder
+    for (b <- bytes) {
+      sb.append(String.format("%02x", Byte.box(b)))
+    }
+    sb.toString
+  }
+
+
   def hashAndNumbers[P](solutionHash: String, rewrites: Seq[Int]): String = {
     val solutionString = solutionHash + rewrites.mkString(":")
 
     val hash = sha256.digest(solutionString.getBytes("UTF-8"))
 
     // return hex string
-    HexFormat.of().formatHex(hash)
+    //    HexFormat.of().formatHex(hash)
+    convertBytesToHex(hash.toSeq)
   }
 
   def hashSolution[P](solution: Solution[P]): String = {
@@ -26,8 +36,9 @@ package object util {
     val hash = sha256.digest(solutionString.getBytes("UTF-8"))
 
     // return hex string
-    HexFormat.of().formatHex(hash)
+    //    HexFormat.of().formatHex(hash)
 
+    convertBytesToHex(hash.toSeq)
   }
 
   def hashProgram[P](program: P): String = {
@@ -37,7 +48,8 @@ package object util {
     val hash = sha256.digest(programString.getBytes("UTF-8"))
 
     // return hex string
-    HexFormat.of().formatHex(hash)
+    //    HexFormat.of().formatHex(hash)
+    convertBytesToHex(hash.toSeq)
   }
 
 }
