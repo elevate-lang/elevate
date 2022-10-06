7bb268f21cbf32c3c860c6802c650eaba4d4c7cc
Johannes Lenfers <j.le@wwu.de>
2022-10-06 19:00:48 +0200

    added limit to local search graph

--- src/main/scala/elevate/heuristic_search/heuristics/LocalSearchGraph.scala
+++ src/main/scala/elevate/heuristic_search/heuristics/LocalSearchGraph.scala
@@ -11,11 +11,13 @@ class LocalSearchGraph[P] extends Heuristic[P] {
     var old_solution = solution
     //    var min: Option[Double] = solutionValue
 
+    var total = 0
+
     val random = scala.util.Random
 
     // later:
     // don't allow duplicates
-    // save position and action
+    // save position and action?
 
     do {
       //get neighbourhood
@@ -23,8 +25,36 @@ class LocalSearchGraph[P] extends Heuristic[P] {
 
       // evaluate neighborhood and get minimum
       old_solution = solution
-      solution = Ns.reduceLeft((a, b) => min(panel, a, b))
-      solutionValue = panel.f(solution)
+      var i = 0
+      println("start inner loop: " + Ns.size)
+      while (i < Ns.size && total < samples) {
+        println("i: " + i)
+
+        val candidate = Ns.apply(i)
+        val candidateValue = panel.f(candidate)
+
+        // check for new minimum
+        val result = candidateValue match {
+          case Some(candidateValue) => solutionValue match {
+            case Some(solutionValue) if candidateValue < solutionValue => (candidate, Some(candidateValue))
+            case Some(solutionValue) => (solution, Some(solutionValue))
+            case None => (candidate, Some(candidateValue))
+          }
+          case None => solutionValue match {
+            case Some(solutionValue) => (solution, Some(solutionValue))
+            case None => (solution, None)
+          }
+        }
+        solution = result._1
+        solutionValue = result._2
+
+        // update counter
+        total += 1
+        i += 1
+      }
+
+      //      solution = Ns.reduceLeft((a, b) => min(panel, a, b))
+      //      solutionValue = panel.f(solution)
 
       // end loop if no new element was found
     } while (!old_solution.equals(solution))
